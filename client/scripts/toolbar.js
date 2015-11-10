71be1b5f3990ff7e7d628fadf83b7ead7ac010d4
Julius Rajala <juliusrajala@gmail.com>
2015-11-10 16:30:44 +0200

    Made some styling changes, added footer etc.

--- client/scripts/toolbar.js
+++ client/scripts/toolbar.js
@@ -5,7 +5,7 @@ NavItem = ReactBootstrap.NavItem;
 
 const navbarInstance = (
   <Navbar className="navBarDark">
-    <NavBrand><a href="#">Skeleton</a></NavBrand>
+    <NavBrand className="navBrand"><a href="#">Skeleton</a></NavBrand>
     <Nav>
       <NavItem className="navigationLink" eventKey={1} href="/images.html"><p className="navItem">Images</p></NavItem>
       <NavItem className="navigationLink" eventKey={2} href="/about.html"><p className="navItem">About</p></NavItem>
